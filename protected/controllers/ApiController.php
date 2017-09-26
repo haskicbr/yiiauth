479cb6c67d595177370ced95dd0090df5492d204
haskicbr <haskicbr@gmail.com>
2017-09-26 17:22:37 +0500

    Update ApiController.php

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -18,24 +18,19 @@
 		public function actionPostDecreaseSum() {
 			$authKey = Yii::app()->request->getQuery('key');
 			$sum = Yii::app()->request->getQuery('sum');
-
-			if(empty($authKey || $sum)) throw new CHttpException('400');
-
 			$user = User::findByAuthKey($authKey);
+			
+			if(empty($user)) throw new CHttpException('400');
 
-			if(!empty($user)) {
-				$user->balance -= $sum;
-				if($user->save()) {
-
-					echo CJSON::encode([
-						'email' => $user->email,
-						'balance' => $user->balance
-					]);
-
-					Yii::app()->end();
-				}
-
-				throw new CHttpException('400');
+			$user->balance -= $sum;
+			if($user->save()) {
+				echo CJSON::encode([
+					'email' => $user->email,
+					'balance' => $user->balance
+				]);
+				Yii::app()->end();
 			}
+			
+			throw new CHttpException('400');
 		}
 	}
